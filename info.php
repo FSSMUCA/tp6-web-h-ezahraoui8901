ad2ea037722177396d65428e86bb58162680b9e2
hindezahraoui37-dot <hindezahraoui37@gmail.com>
2025-12-11 15:24:14 +0100

    index.html

---
+++ info.php
@@ -0,0 +1,34 @@
+ <?php
+$etablissement = "FSSM Semlalia";
+$module = "Programmation Web";
+$annee = 2025;
+$a = 7;
+$b = 3;
+$addition = $a + $b;
+$multiplication = $a * $b;
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Info PHP</title>
+</head>
+<body>
+  <div class="container">
+    <h1> Informations PHP</h1>
+    <div class="card">
+      <p><strong>Établissement :</strong> <?php echo $etablissement; ?></p>
+      <p><strong>Module :</strong> <?php echo $module; ?></p>
+      <p><strong>Année :</strong> <?php echo $annee; ?></p>
+      <hr>
+      <p><strong>Résultat addition :</strong> <?php echo "$a + $b = $addition"; ?></p>
+      <p><strong>Résultat multiplication :</strong> <?php echo "$a * $b = $multiplication"; ?></p>
+    </div>
+    <div class="back-link">
+      <a href="index.html"> Retour à la calculatrice</a>
+    </div>
+  </div>
+</body>
+</html>

2431036d4df4a4e1a511243ec8edd382bfe64da9
hindezahraoui37-dot <hindezahraoui37@gmail.com>
2025-12-11 15:42:28 +0100

    info.php

--- info.php
+++ info.php
@@ -13,7 +13,7 @@ $multiplication = $a * $b;
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Info PHP</title>
+  <title>Informations PHP</title>
 </head>
 <body>
   <div class="container">
